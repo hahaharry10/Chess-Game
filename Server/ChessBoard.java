00b79911ac79b475f67554d9f2e44c5973819081
Harry Nixon <sc21hn@leeds.ac.uk>
2023-09-01 20:23:17 +0100

    bug fix: git commit #084eae4f3e6cf659f81b26a636ea397494c6c4b4 was incorrect, the piece colour notation inconsistency has now been rectified

--- Server/ChessBoard.java
+++ Server/ChessBoard.java
@@ -8,8 +8,8 @@ public class ChessBoard
     private int boardWidth = 10;
 
     final char emptyTile = '·';
-    final char black = 1;
-    final char white = 2;
+    final char black = 1; // Denoted as an upper case character.
+    final char white = 2; // Denoted as a lower case character
 
     /**
      * Initialise the board with all the pieces in their starting positions.
@@ -45,7 +45,7 @@ public class ChessBoard
         // The peices will be represented by the starting letter of the piece (i.e. pawn will be shown with a 'P' or 'p').
         // The characters will either be in capital or lower case; capital case represents white, lowercase represents black.
 
-        // place all white pieces onto the board:
+        // place all black pieces onto the board:
         for (int i = 1; i < 9; i++)
             board[2][i] = 'P'; // Pawns
         board[1][1] = 'R'; board[1][8] = 'R'; // Rooks
@@ -54,7 +54,7 @@ public class ChessBoard
         board[1][4] = 'Q'; // Queen
         board[1][5] = 'K'; // King
 
-        // place all black pieces onto the board:
+        // place all white pieces onto the board:
         for (int i = 1; i < 9; i++)
             board[7][i] = 'p'; // Pawns
         board[8][1] = 'r'; board[8][8] = 'r'; // Rooks
@@ -176,9 +176,9 @@ public class ChessBoard
     private int getColourOfPiece(char piece)
     {
         if (Character.isUpperCase(piece))
-            return white;
-        else if (Character.isLowerCase(piece))
             return black;
+        else if (Character.isLowerCase(piece))
+            return white;
         else
             return 0;
     }
@@ -752,7 +752,7 @@ public class ChessBoard
             return 0;
         else if (kingCanEscapeCheck(forWhite, kingsLoc) || checkCanBeObstructed(forWhite, kingsLoc)) // If check can be avoided next move.
             return 1;
-        else // If the player is in check and check cannot be avoided.
+        else // If the player is in checkmate.
             return 2;
     }
     
